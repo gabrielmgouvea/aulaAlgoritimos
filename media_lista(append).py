c8305d00b85360d3c4de3edfe16d4587e44f1f4f
gabrielmgouvea <bielgouvea.rj@gmail.com>
2024-08-13 22:09:32 -0300

    Add files via upload

---
+++ media_lista(append).py
@@ -0,0 +1,10 @@
+lista = []
+n = 10
+#for i in range(n+1):
+    #lista.append(i)
+    #print(lista[i])
+lista =list(range(1,1001))
+for i in lista:
+    print(i)
+media = sum(lista)/len(lista)
+print(media)
