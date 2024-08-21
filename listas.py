6facfed0f513dc22fe09fe22c670edae14847afd
gabrielmgouvea <bielgouvea.rj@gmail.com>
2024-08-20 22:04:05 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,65 @@
+lista = [10, 20, 30, 40]
+i = 0
+while i < len(lista):   
+    print(lista[i])
+    i += 1
+
+
+#     Looping with a Condition
+# fruits = ["apple", "banana", "orange", "grape"]
+# index = 0
+# while index < len(fruits):
+# 	if len(fruits[index]) > 5:
+# 		print(fruits[index])
+# 	index += 1
+
+
+
+#  Modifying List Elements
+# numbers = [1, 2, 3, 4, 5]
+# index = 0
+# while index < len(numbers):
+# 	numbers[index] *= 2 # Multiply each element by 2
+# 	index += 1
+# print(numbers)
+
+
+
+#  User Input in Loop
+# user_list = []
+# user_input = input("Enter a number (type 'done' to finish): ")
+# while user_input.lower() != 'done':
+# 	user_list.append(int(user_input))
+# 	user_input = input("Enter another number (type 'done' to finish): ")
+# print("User's List:", user_list)
+
+
+
+
+
+
+"""
+lista[0] = 100
+print(lista)
+lista.append(555)
+print(lista)
+lista.insert(2, 777)
+print(lista)
+for i in lista:
+    print(i)
+lista.insert(1, 777)
+lista.remove(777)
+lista.remove(777)
+lista.pop()
+print(lista)
+lista.clear()
+print(lista)
+
+"""
+"""
+print(lista)
+for i in range(len(lista)):
+    print(lista[i])
+
+"""
+
